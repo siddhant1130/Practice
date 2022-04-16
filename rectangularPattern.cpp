0e1eb24ec588c80a6e85016cc6224b7eb2d9e9e7
Satkar Siddhant <81086661+siddhant1130@users.noreply.github.com>
2022-04-16 21:46:54 +0530

    Add files via upload

---
+++ rectangularPattern.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int row,col;
+	cin>>row>>col;
+	int i,j;
+	
+	for(i=1;i<=row;i++)
+	{
+		for(j=1;j<=col;j++)
+		{
+			cout<<" * ";
+		}
+		cout<<endl;
+	}
+}
